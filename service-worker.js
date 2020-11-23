53cd6743bea8dc12a1e2b85af39e2fa3c4e136a5
lawalalao <lawalalaoad@gmail.com>
2020-11-23 15:11:22 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0c39fa5099815299e55f3ecf072ed152.js"
+  "./precache-manifest.9eea8e035eff3cffacdf1b9591787bfb.js"
 );
 
 self.addEventListener('message', (event) => {
